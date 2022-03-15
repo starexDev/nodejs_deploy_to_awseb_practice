f34e4f0c4ffd2f53d0a3069b990a93b47d150594
Mickey <mickey88661@google.com>
2022-03-15 17:40:44 +0800

    mail

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const nodemailer = require("nodemailer");
+
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -10,6 +12,33 @@ app.post('/test', (req, res) => {
     res.status(200).json({
         envport: `${process.env.PORT}`,
         port: port
+    });
+})
+
+app.post('/mail', async (req, res) => {
+    let testAccount = await nodemailer.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    });
+
+    res.status(200).json({
+        message_sent: info.messageId,
+        preview_url: nodemailer.getTestMessageUrl(info)
     });
 })
 

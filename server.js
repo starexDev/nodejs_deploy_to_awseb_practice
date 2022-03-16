8785b078a89d77a46a7ea4ba09721a5cea4710ac
Mickey <mickey88661@google.com>
2022-03-16 15:42:02 +0800

    test app-module-path

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+// ***IMPORTANT**: The following line should be added to the very
+//                 beginning of your main script!
+require('app-module-path').addPath(__dirname);
+
 const express = require('express');
 require('dotenv').config()
 const nodemailer = require("nodemailer");
 
-const emailValidationRoute = require("./Routes/Validation/email_validation.js");
+const emailValidationRoute = require("Routes/Validation/email_validation");
 
 const app = express();
 const port = process.env.PORT || 8080;

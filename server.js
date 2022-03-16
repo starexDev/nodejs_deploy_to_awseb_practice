3a682e563a52afb9d60680729ff51d4ca4b6c0ef
Mickey <mickey88661@google.com>
2022-03-16 15:54:36 +0800

    test naming

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 require('dotenv').config()
 const nodemailer = require("nodemailer");
 
-const emailValidationRoute = require("Routes/Validation/email_validation");
+const emailValidationRoute = require("routes/validation/email_validation");
 
 const app = express();
 const port = process.env.PORT || 8080;
